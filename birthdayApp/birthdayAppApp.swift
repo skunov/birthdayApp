0edc722186726bafdd284884e2ef664e55b69cff
Sophia Kunov <kunov.sophia@gmail.com>
2025-08-08 11:53:08 -0700

    Initial Commit

---
+++ birthdayApp/birthdayAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  birthdayAppApp.swift
+//  birthdayApp
+//
+//  Created by Scholar on 8/8/25.
+//
+
+import SwiftUI
+
+@main
+struct birthdayAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
